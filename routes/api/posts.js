45701a00c769941ff489138cc126eb5b935e9c5b
Shakeeb Sheikh <shakeebaftab@gmail.com>
2020-07-07 04:52:49 +0500

    Fetch single post and delete post added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -39,10 +39,37 @@ router.post(
             await newPost.save();
             res.json(newPost);
         } catch (error) {
-            console.log(error.message);
+            console.error(error.message);
             res.status(500).send(`Server Error`);
         }
     }
 );
+
+//  PRIVATE GET api/posts/:postId :: GET SINGLE POST
+router.get('/:postId', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        post
+            ? res.json({ post })
+            : res.status(404).json({ msg: 'Post was not found' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(`Server Error`);
+    }
+});
+
+//  PRIVATE DELETE api/users/:postId :: DELETE A POST
+router.delete('/:postId', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        post.user.toString() != req.user.id
+            ? res.status(400).json({ msg: 'Access denied' })
+            : await post.remove(),
+            res.json({ msg: 'Post Removed' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(`Server Error`);
+    }
+});
 
 module.exports = router;
