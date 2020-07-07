ad260642a410663d598f594c66e799ec22e28cee
Shakeeb Sheikh <shakeebaftab@gmail.com>
2020-07-07 16:12:23 +0500

    Add and Delete Comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,11 +8,12 @@ const auth = require('../../middleware/auth');
 //  Modals
 const User = require('../../modals/User');
 const Post = require('../../modals/Post');
+const { compareSync } = require('bcryptjs');
 
 //  PRIVATE GET api/posts :: GET ALL POSTS
 router.get('/', auth, async (req, res) => {
     try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ date: -1 });
         res.json(posts);
     } catch (error) {
         console.log(error.message);
@@ -68,6 +69,59 @@ router.delete('/:postId', auth, async (req, res) => {
             res.json({ msg: 'Post Removed' });
     } catch (error) {
         console.error(error.message);
+        res.status(500).send(`Server Error`);
+    }
+});
+
+//  PRIVATE PUT api/posts/comments/:postId :: CREATE A NEW COMMENT
+router.put(
+    '/comments/:postId',
+    [auth, [check('body', 'Comment body is required').not().isEmpty()]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ error: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id);
+            const post = await Post.findById(req.params.postId);
+            const newComment = {
+                user: req.user.id,
+                name: user.name,
+                body: req.body.body,
+            };
+            post.comments.unshift(newComment);
+            await post.save();
+            res.json(post.comments);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send(`Server Error`);
+        }
+    }
+);
+
+//  PRIVATE DELETE api/posts/comments/:postId/:commentId :: DELETE A COMMENT
+router.delete('/comments/:postId/:commentId', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        let isMatch = false;
+        post.comments.map((comment) => {
+            if (comment.user.toString() == req.user.id) {
+                isMatch = true;
+            }
+        });
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Access Denied' });
+        }
+        const updatedComments = [];
+        post.comments.map((comment) => {
+            comment.id != req.params.commentId && updatedComments.push(comment);
+        });
+        post.comments = updatedComments;
+        await post.save();
+        res.json(post.comments);
+    } catch (error) {
+        console.log(error.message);
         res.status(500).send(`Server Error`);
     }
 });
